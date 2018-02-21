c404b5b9d0ac37363c82c6a04d01cc1193a9f6f9
Andrew Cramp <Andrew Cramp>
2018-02-21 00:42:57 -0500

    Add google maps to UI

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,33 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+import { Tutors } from '../api/tutors.js';
 import './tutor.js';
 import './body.html';
 
 Template.body.helpers({
-  tutors : [
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-    { text: 'tutor 1'},
-    { text: 'tutor 2' },
-  ],
+  tutors(){
+    return Tutors.find({});
+  },
 });
